9277c9cfaba9ccaf6cac07c10e4849ce6d9b42c0
dragomirsimina97 <44194285+dragomirsimina97@users.noreply.github.com>
2021-03-03 20:12:59 +0200

    Delete db directory

--- db/seeds.rb
+++
@@ -1,9 +0,0 @@
-# Seed the RottenPotatoes DB with some bikes.
-more_bikes = [
-  {:Bike_ID => 'Bike_0001',:State => 'Free', :Station => 'Vivo',
-    :Time => '25-Nov-2019'}
-]
-
-more_bikes.each do |bik|
-  Bike.create!(bik)
-end
